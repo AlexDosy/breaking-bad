89049a48cd782f96ed0045d9e7f7259e538b8cde
AlexDosy <alexdosyv210@gmail.com>
2021-09-16 08:09:10 +0530

    Initial

---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CharcterItem from "./CharcterItem";
+import Spinner from "../ui/Spinner";
+const CharacterGrid = ({ items, isLoading }) => {
+	return isLoading ? (
+		<Spinner />
+	) : (
+		<section className="cards">
+			{items.map((item) => (
+				<CharcterItem key={item.char_id} item={item}></CharcterItem>
+			))}
+		</section>
+	);
+};
+
+export default CharacterGrid;
